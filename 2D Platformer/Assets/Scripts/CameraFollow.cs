802b5270522967cb4039c4282eedb5317b4a9c17
Tajinder <tnijjar2@my.centennialcollege.ca>
2025-06-09 19:05:46 -0400

    Add skybox and other assets from unity store

--- 2D Platformer/Assets/Scripts/CameraFollow.cs
+++ 2D Platformer/Assets/Scripts/CameraFollow.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float height = 2;
+    public float heightOffset = 2;
     public float depth = -10;
 
     private void LateUpdate()
     {
         transform.position = new Vector3(
             target.position.x,
-            height,
+            target.position.y + heightOffset,
             depth
             );
     }
